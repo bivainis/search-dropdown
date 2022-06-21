017ce489735e2918896c10125db26e9e646cc550
Gediminas Bivainis 💜 <gediminas@bivainis.com>
2022-06-21 23:04:45 +0200

    Add basic data fetching, show/hide on focus/blur

--- src/components/live-search/LiveSearch.test.tsx
+++ src/components/live-search/LiveSearch.test.tsx
@@ -1,4 +1,5 @@
-import { cleanup, render, screen } from '@testing-library/react';
+import { cleanup, render, screen, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import LiveSearch from './LiveSearch';
 
 beforeEach(() => {
@@ -11,4 +12,33 @@ afterEach(() => {
 
 test('Renders select component without errors.', () => {
   expect(screen.getByRole('textbox')).toBeInTheDocument();
+});
+
+test('User can focus on the input field', () => {
+  const inputElement = screen.getByRole('textbox');
+
+  userEvent.click(inputElement);
+
+  expect(inputElement).toHaveFocus();
+});
+
+test('Dropdown is hidden on initial render', () => {
+  const dropdownElementOrNull = screen.queryByRole('list');
+
+  expect(dropdownElementOrNull).not.toBeInTheDocument();
+});
+
+test('When user clicks into the input field, he/she sees the full list of managers.', async () => {
+  const inputElement = screen.getByRole('textbox', { name: 'Manager search' });
+
+  userEvent.click(inputElement);
+
+  const list = await screen.findByRole('list');
+
+  expect(list).toBeInTheDocument();
+
+  const { getAllByRole } = within(list);
+  const items = getAllByRole('listitem');
+
+  expect(items.length).toBeGreaterThan(1);
 });
--- src/components/live-search/LiveSearch.tsx
+++ src/components/live-search/LiveSearch.tsx
@@ -1,17 +1,115 @@
+import { ChangeEvent, useEffect, useState } from 'react';
+import { API_URL } from '../../urls';
 import styles from './LiveSearch.module.css';
 
 interface LiveSearchProps {
   id: string;
 }
 
+interface Employee {
+  id: number;
+  attributes: {
+    avatar: string;
+    firstName: string;
+    lastName: string;
+  };
+}
+
+/**
+ * @TODO
+ * - tests
+ * - test utils
+ * - abstract fetching to a hook
+ * - aria roles: listbox > option: https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/listbox_role
+ * - esc should clear the field
+ * - keyboard navigation
+ * - debounce
+ * - styling
+ * - documentation and sources
+ * - lock node version
+ * - api url env variable
+ * - (maybe) cache results and compare when rendering
+ * - (maybe) look into suspense
+ * - (maybe) paginate
+ * - (maybe) storybook
+ * - (maybe) animations
+ * - (maybe) add params on how much to fetch
+ */
 const LiveSearch = ({ id }: LiveSearchProps) => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [dropdownIsOpen, setDropdownIsOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<Error>();
+  const [data, setData] = useState<Employee[]>([]);
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const handleInputFocus = () => {
+    setDropdownIsOpen((current) => !current);
+  };
+
+  useEffect(() => {
+    const fetchEmployees = async () => {
+      setLoading(true);
+
+      try {
+        const res = await fetch(API_URL);
+        const json = await res.json();
+
+        setData(json.data);
+      } catch (error) {
+        if (error instanceof Error) {
+          setError(error);
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchEmployees();
+  }, []);
+
   return (
     <div className={styles.container}>
-      <label htmlFor={id}></label>
-      <input id={id} type="text" />
+      <label htmlFor={id}>Manager search</label>
+
+      <input
+        className={styles.input}
+        id={id}
+        type="text"
+        value={searchQuery}
+        onChange={handleInputChange}
+        onFocus={handleInputFocus}
+        onBlur={handleInputFocus}
+      />
+
+      {loading && <p>Loading...</p>}
+
+      {error && <p>An error occurred</p>}
+
+      {dropdownIsOpen && !loading && data.length > 0 && (
+        <ul className={styles.dropdownList}>
+          {data.map(({ id, attributes }) => {
+            return (
+              <li key={id}>
+                {attributes.avatar !== null && (
+                  <img
+                    src={attributes.avatar}
+                    alt={`${attributes.firstName} ${attributes.lastName} avatar image`}
+                  />
+                )}
+                {attributes.firstName} {attributes.lastName}
+                <br />
+                {/* @TODO: email */}
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 };
 
 export default LiveSearch;
-gst;
